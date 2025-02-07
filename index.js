4062bfd3277e51a68e57a3d9e4e8a53826861405
alexavil <alexavil007@gmail.com>
2025-02-07 17:20:51 +0000

    ground work

---
+++ index.js
@@ -0,0 +1,46 @@
+require("dotenv/config");
+
+const TelegramBot = require("node-telegram-bot-api");
+const OpenAI = require("openai");
+
+const openai = new OpenAI({
+    baseURL: "https://openrouter.ai/api/v1",
+    apiKey: process.env.OPENROUTER_API_KEY,
+})
+
+const bot = new TelegramBot(process.env.TOKEN, {
+    polling: true,
+    onlyFirstMatch: true,
+});
+
+bot.onText(/\/start/, (msg, match) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(
+        chatId, `Добро пожаловать! Данный бот поможет вам с применением ФГОС.`
+    );
+});
+
+bot.onText(/\/faq/, (msg, match) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(
+        chatId, `В разработке!`
+    );
+});
+
+bot.onText(/\/askai/, async (msg, match) => {
+    const chatId = msg.chat.id;
+    const completion = await openai.chat.completions.create({
+        model: "deepseek/deepseek-r1:free",
+        messages: [
+          { role: "user", content: "What is the meaning of life?" }
+        ],
+      })
+    console.log(completion.choices[0].message)
+    bot.sendMessage(
+        chatId, completion.choices[0].message.content
+    );
+});
+
+bot.on("polling_error", (err) => {
+    console.log(err)
+});
